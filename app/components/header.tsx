91bf16d16cb632ce7b2ca46b073873289e4296cd
Chapman Kwan <chapman7kwan@gmail.com>
2025-05-06 20:47:48 -0700

    minor changes to the header

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -7,10 +7,10 @@ export const Header = () => {
 
     return (
         <header className="sticky top-0 z-10 px-6 py-4 w-full flex items-center justify-between bg-slate-700">
-            <Link href="/">Home</Link>
-            <section className="flex gap-10 *:cursor-pointer *:hover:underline *:hover:underline-offset-4">
-                <Link href={`/todo-lists/${todaysDate}`}>Task list</Link>
-                <Link href="/tracker">Tracker</Link>
+            <Link href="/">Monorail</Link>
+            <section className="flex gap-10 *:cursor-pointer *:hover:underline *:hover:underline-offset-4 text-xs">
+                <Link href={`/todo-lists/${todaysDate}`}>Tasks</Link>
+                <Link href="/tracker">Workspaces</Link>
                 {
                     !user ? 
                     <Link href="/account/login">Login</Link> :
