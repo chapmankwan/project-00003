d6c1220e09a6b48858b853fd208972fe34c60a6f
Chapman Kwan <chapman7kwan@gmail.com>
2025-06-16 15:34:28 -0700

    more visual updates

--- app/components/card.tsx
+++ app/components/card.tsx
@@ -3,6 +3,7 @@ import Link from "next/link";
 import type { TodoListModel } from "@/app/models"
 
 import { CheckCircleIcon } from "@heroicons/react/24/outline";
+import clsx from "clsx";
 
 interface CardModel {
     taskList: TodoListModel
@@ -11,22 +12,24 @@ interface CardModel {
 export const Card = ({
     taskList
 }: CardModel) => {
+    
+    const completedTasks = taskList.tasks.filter( task => task.completed).length;
+    const totalNumberOfTasks = taskList.tasks.length;
 
     return (
         <Link 
             href={`/todo-lists/${taskList.id}/${taskList.slug}`}
-            className="
-                first:mt-0 last:mb-0 my-3 p-3 flex h-16
-                items-center justify-between cursor-pointer 
-                rounded-sm gap-3
-                bg-slate-700 hover:bg-slate-600"
+            className={clsx(completedTasks === totalNumberOfTasks
+                    ? "bg-lime-900 hover:bg-lime-800" 
+                    : "bg-slate-700 hover:bg-slate-600", 
+                    "first:mt-0 last:mb-0 my-3 p-3 flex h-16 items-center justify-between cursor-pointer rounded-sm gap-3")}
         >
             <span>{taskList.title}</span>
             {
                 taskList.tasks.length > 0 ?
                 <span className="flex items-center gap-2">
                     <CheckCircleIcon className="size-5" />
-                    <p className="font-semibold">{taskList.tasks.filter( task => task.completed).length} / {taskList.tasks.length}</p>
+                    <p className="font-semibold">{completedTasks} / {totalNumberOfTasks}</p>
                     <p className="hidden md:block">completed</p>
                 </span>
                 :
